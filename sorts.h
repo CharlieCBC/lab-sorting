9021ab14c91683a1cbbbefaa020a8f8162051438
CharlieCBC <chang.bin@northeastern.edu>
2023-02-07 19:47:45 -0500

    Develop bubble sort function and swap function

--- sorts.h
+++ sorts.h
@@ -20,6 +20,18 @@
 void bubbleSortIntegers(int *array, unsigned int size, int print)
 {
    // TODO implement
+   int i;
+   int j;
+   for (i = 0; i < size - 1; i++)
+   {
+    for (j = 0; j < size - i - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            swap(&array[j], &array[j + 1]);
+        }
+    }
+   }
 }
 
 
